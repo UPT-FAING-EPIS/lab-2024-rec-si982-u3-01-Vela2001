a646c60511a34b0ce33d33987bd28c7f1faa91a0
Vela2001 <81716261+Vela2001@users.noreply.github.com>
2025-02-19 23:30:46 -0500

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,3 @@
-# Configure the Azure provider
 terraform {
   required_providers {
     azurerm = {
@@ -8,9 +7,25 @@ terraform {
   }
   required_version = ">= 0.14.9"
 }
+
+variable "suscription_id" {
+    type = string
+    description = "Azure subscription id"
+}
+
+variable "sqladmin_username" {
+    type = string
+    description = "Administrator username for server"
+}
+
+variable "sqladmin_password" {
+    type = string
+    description = "Administrator password for server"
+}
+
 provider "azurerm" {
   features {}
-  subscription_id = "ID SUSCRIPCION AZURE"
+  subscription_id = var.suscription_id
 }
 
 # Generate a random integer to create a globally unique name
@@ -22,7 +37,15 @@ resource "random_integer" "ri" {
 # Create the resource group
 resource "azurerm_resource_group" "rg" {
   name     = "upt-arg-${random_integer.ri.result}"
-  location = "eastus"
+  location = "eastus2"
+}
+
+resource "azurerm_storage_account" "storageaccount" {
+  name                     = "uptasa${random_integer.ri.result}"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
 # Create the Linux App Service Plan
@@ -31,22 +54,49 @@ resource "azurerm_service_plan" "appserviceplan" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
-  sku_name            = "F1"
+  sku_name            = "Y1"
 }
 
-# Create the web app, pass in the App Service Plan ID
-resource "azurerm_linux_web_app" "webapp" {
-  name                  = "upt-awa-${random_integer.ri.result}"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  service_plan_id       = azurerm_service_plan.appserviceplan.id
-  depends_on            = [azurerm_service_plan.appserviceplan]
-  //https_only            = true
+resource "azurerm_linux_function_app" "azurefunction" {
+  name                = "upt-afn-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  storage_account_name       = azurerm_storage_account.storageaccount.name
+  storage_account_access_key = azurerm_storage_account.storageaccount.primary_access_key
+  service_plan_id            = azurerm_service_plan.appserviceplan.id
   site_config {
     minimum_tls_version = "1.2"
     always_on = false
     application_stack {
       dotnet_version = "8.0"
-    }
+      }
   }
+}
+
+resource "azurerm_static_web_app" "example" {
+  name                = "upt-swa-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_mssql_server" "sqlsrv" {
+  name                         = "upt-dbs-${random_integer.ri.result}"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = var.sqladmin_username
+  administrator_login_password = var.sqladmin_password
+}
+
+resource "azurerm_mssql_firewall_rule" "sqlaccessrule" {
+  name             = "PublicAccess"
+  server_id        = azurerm_mssql_server.sqlsrv.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "255.255.255.255"
+}
+
+resource "azurerm_mssql_database" "sqldb" {
+  name      = "shorten"
+  server_id = azurerm_mssql_server.sqlsrv.id
+  sku_name = "Free"
 }
